c8b491bc1310cc8692c6d70a60b8b2d868e9112d
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2023-10-11 08:32:29 -0300

    alteração do select

--- processamento_login.php
+++ processamento_login.php
@@ -5,11 +5,11 @@
             $username = $_POST['username']; /*name do email-input*/
             $senha = $_POST['password']; /*name do password-input*/
             
-            $sql = "SELECT username, password FROM usuario WHERE username_bombeiro = '$username' && password_bombeiro = '$senha'"; /*informações de tabela e campos de acordo como seu BD*/
+            $sql = "SELECT username_bombeiro, password_bombeiro FROM usuario WHERE username_bombeiro = '$username' && password_bombeiro = '$senha'"; /*informações de tabela e campos de acordo como seu BD*/
             $result = mysqli_query($conn, $sql);
             
             if (mysqli_num_rows($result) > 0) { 
-              header("Location: https://www.google.com"); /*local para onde deseja redirecionar o usuário*/
+              header("Location: home.html"); /*local para onde deseja redirecionar o usuário*/
             } else {
                 header("Location: login.html"); /*local para onde deseja redirecionar o usuário*/
                 /*echo "<script>document.querySelector('#form-text').innerText = 'E-mail ou senha inválidos'</script>";*/
