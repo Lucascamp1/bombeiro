a48e745c868377c08592035330e6f3ae133b9d5e
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2023-11-30 11:25:00 -0300

    first

--- cadastro.php
+++ cadastro.php
@@ -7,11 +7,12 @@
     $email = $_POST['email'];
     $senha = $_POST['senha'];
     $matricula = $_POST['matricula'];
-    $genero = $_POST['genero'];
     $dataNascimento = $_POST['dataNascimento'];
     
-    $sql = "INSERT INTO usuario (nome_bombeiro, matricula_bombeiro, username_bombeiro, email_bombeiro, data_nascimento_bombeiro, password_bombeiro, genero_bombeiro ) 
-    VALUES ('$nome', '$matricula', '$username', '$email', '$dataNascimento', '$senha', '$genero')";
+    $sql = "INSERT INTO usuario (nome_bombeiro, matricula_bombeiro, username_bombeiro, email_bombeiro, data_nascimento_bombeiro, password_bombeiro); 
+    VALUES ('$nome', '$matricula', '$username', '$email', '$dataNascimento', '$senha',)";
+
+    $result = mysqli_query($conn, $sql);
 
     if ($conn->query($sql) === TRUE) {
         header("Location: login.html");
