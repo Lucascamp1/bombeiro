5f476a2143c1db0b5b26873e1bfbd1938ce07776
Lucas <lucas.campestrini21@gmail.com>
2023-12-03 18:27:54 -0300

    first

--- pages/editorphp.php
+++ pages/editorphp.php
@@ -171,8 +171,8 @@ include('connect_usuarios.php');
             </a>
         </div>
     </header>
-<br>
-<br>
+    <br>
+    <br>
     <?php
     if ($editUsuarioId !== null) {
         // Recupera os dados do usuário com base no ID fornecido para edição
@@ -298,5 +298,9 @@ include('connect_usuarios.php');
     </script>
 
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
 
 </html>
